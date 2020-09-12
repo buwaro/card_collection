05cce536d3fbd91eb04cad1541f37fb59bd2de63
Buwaro <buwaro@gmail.com>
2020-09-12 16:56:26 +0200

    deploy

---
+++ precache-manifest.32130b708b50d94d7cd4d61175698a35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aca5e23120a4c82f914",
+    "url": "/card_collector/css/app.8297280c.css"
+  },
+  {
+    "revision": "d618494500a6b5354eb0bf217f2eac21",
+    "url": "/card_collector/img/mtg_card_back.d6184945.jpg"
+  },
+  {
+    "revision": "405eb3cf0b38b4c8fff01a60dc20a3ef",
+    "url": "/card_collector/index.html"
+  },
+  {
+    "revision": "2aea83d32eb363a22dc9",
+    "url": "/card_collector/js/about.bd05e773.js"
+  },
+  {
+    "revision": "1aca5e23120a4c82f914",
+    "url": "/card_collector/js/app.a360f5da.js"
+  },
+  {
+    "revision": "db103715773354db3401",
+    "url": "/card_collector/js/chunk-vendors.0bc56e93.js"
+  },
+  {
+    "revision": "70a77991d53c140850be3ee612bde02e",
+    "url": "/card_collector/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/card_collector/robots.txt"
+  }
+]);
